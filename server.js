f8742cbc985571bfe46e3d3213c1856b7987bd95
Yuko Ishida <yishida2@my.bcit.ca>
2023-01-17 14:48:35 -0800

    ignore image

--- server.js
+++ server.js
@@ -1,30 +1,49 @@
 const express = require('express')
 const fs = require('fs')
 const multer = require('multer')
+require('dotenv').config()
+const database = require("./database.js");
 
 const upload = multer({ dest: 'images/' })
 
 const app = express()
 
 // app.use('/images', express.static('images'))
-app.get('/images/:imageName', (req, res) => {
-  // do a bunch of if statements to make sure the user is 
-  // authorized to view this image, then
 
-  const imageName = req.params.imageName
-  const readStream = fs.createReadStream(`images/${imageName}`)
-  readStream.pipe(res)
-})
 
-app.post('/api/images', upload.single('image'), (req, res) => {
+app.post('/api/images', upload.single('image'), async(req, res) => {
   const imagePath = req.file.path
   const description = req.body.description
 
   // Save this data to a database probably
+  const data = req.body
+  console.log(imagePath)
+  await database.addImage(imagePath,description)
 
   console.log(description, imagePath)
   res.send({description, imagePath})
 })
+
+// Get a list of all the images from the database
+app.get("/api/images", async(req, res) => {
+  const images = await database.getImages();
+  // console.log(images)
+  res.send({images: images})
+})
+
+// Get the single image file, given the file path
+// app.get('/api/images/images/:imageName', (req, res) => {
+  app.get('/api/image/*', (req, res) => {
+    // do a bunch of if statements to make sure the user is 
+    // authorized to view this image, then
+  
+    //console.log("TEST", req.body);
+    const imagePath = req.params[0]
+    // console.log(imagePath);
+    const readStream = fs.createReadStream(imagePath)
+    readStream.pipe(res)
+  })
+  
 
 const port = process.env.PORT || 8080
 app.listen(8080, () => console.log("listening on port 8080"))
