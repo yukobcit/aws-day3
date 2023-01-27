c98f9a840c06bd2e3cfc4cf2ceec0e745d6cd0a1
Yuko Ishida <yishida2@my.bcit.ca>
2023-01-27 15:23:20 -0800

    server info

--- database.js
+++ database.js
@@ -10,14 +10,11 @@ const pool = mysql
     // user: process.env.MYSQL_USER,
     // password: process.env.MYSQL_PASSWORD,
     // database: process.env.MYSQL_DATABASE,
-
- 
-    port: process.env.MYSQLPORT || 3306,
-
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
   })
   .promise()
 
