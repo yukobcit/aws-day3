723caa79978cdd96c6f8266e01e6ce94c00bc75c
Yuko Ishida <yishida2@my.bcit.ca>
2023-01-27 14:50:55 -0800

    env

--- database.js
+++ database.js
@@ -6,10 +6,14 @@ dotenv.config()
 
 const pool = mysql
   .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    // host: process.env.MYSQL_HOST,
+    // user: process.env.MYSQL_USER,
+    // password: process.env.MYSQL_PASSWORD,
+    // database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
   })
   .promise()
 
