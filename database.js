25eb192b62673bcf76b6f22ca21691ddd89e25bc
Yuko Ishida <yishida2@my.bcit.ca>
2023-01-27 15:37:32 -0800

    take away port

--- database.js
+++ database.js
@@ -14,7 +14,7 @@ const pool = mysql
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT || 3306,
+
   })
   .promise()
 
